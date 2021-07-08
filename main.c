3fc135e02dcbf02eb4ad9543ae853602adbd53db
Ian D <43609709+haharkagain@users.noreply.github.com>
2021-07-08 03:50:26 -0400

    optimizations, fix warnings

--- main.c
+++ main.c
@@ -50,6 +50,27 @@ int initBoard[9][9] = {
 	{0, 0, 3, 4, 0, 0, 0, 0, 0},
 	{0, 0, 0, 2, 0, 0, 6, 0, 0}
 };
+/*
+	{8, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 3, 6, 0, 0, 0, 0, 0},
+	{0, 7, 0, 0, 9, 0, 2, 0, 0},
+	{0, 5, 0, 0, 0, 7, 0, 0, 0},
+	{0, 0, 0, 0, 4, 5, 7, 0, 0},
+	{0, 0, 0, 1, 0, 0, 0, 3, 0},
+	{0, 0, 1, 0, 0, 0, 0, 6, 8},
+	{0, 0, 8, 5, 0, 0, 0, 1, 0},
+	{0, 9, 0, 0, 0, 0, 4, 0, 0}
+
+	{0, 0, 0, 8, 0, 1, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 4, 3, 0},
+	{5, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 7, 0, 8, 0, 0},
+	{0, 0, 0, 0, 0, 0, 1, 0, 0},
+	{0, 2, 0, 0, 3, 0, 0, 0, 0},
+	{6, 0, 0, 0, 0, 0, 0, 7, 5},
+	{0, 0, 3, 4, 0, 0, 0, 0, 0},
+	{0, 0, 0, 2, 0, 0, 6, 0, 0}
+*/
 
 void WinMain(int arg, char *argv[]) {
     if (!init()) return;	// initialize the video, window, and renderer
@@ -181,7 +202,6 @@ int eventHandler(SDL_Event e) {	// handle events
 	} else if(e.type == SDL_KEYDOWN) {	// keydown event
 		if (drawMode) return 0;			// dont take events while solving
 		switch(e.key.keysym.sym) {
-			SDL_Thread* solveT;
 			case SDLK_UP:				// up
 				if (posY > 0) posY--; 	// check if on the edge of the board
 				break;
@@ -261,7 +281,7 @@ void close() {		// deallocate memory and close the window
 	int i;	
 	for (i=1; i < 9; i++) {			
 		SDL_DestroyTexture(numbers[i]);	// free number textures
-		numbers[i] == NULL;
+		numbers[i] = NULL;
 	}
 	SDL_DestroyRenderer(renderer);	// free renderer
 	renderer = NULL;
--- start.bat
+++ start.bat
@@ -1,7 +1,13 @@
-g++ main.c SDL2.dll
-if exist "a.exe" (
-	a.exe
-	del a.exe
+@ECHO OFF
+if exist "main.exe" (
+	del main.exe
+)
+gcc main.c SDL2.dll -o main.exe -O3 -Wall
+if exist "main.exe" (
+	echo Compiled, running...
+	main.exe
+	del main.exe
 ) else (
+	echo.
 	pause
 )
